38f5b7206f609490374466bd46086d7b486c9554
raldios <david@lademan.net>
2020-10-12 10:54:10 -0400

    remove dailies crud

--- src/Bot.py
+++ src/Bot.py
@@ -8,9 +8,7 @@ from discord.ext import commands, tasks
 from pathlib import Path
 import asyncio
 
-from cogs.DailiesManagementCog import DailiesManagementCog
 from cogs.FactsCog import FactsCog
-from Embeds import embeds
 
 
 class Bot(commands.Bot):
@@ -20,15 +18,8 @@ class Bot(commands.Bot):
 
         self.token = token
         self.guild_name = guild_name
-        self.timezone = ZoneInfo(timezone)
-        self.json_path = Path.cwd().parent / json_folder
-        if not self.json_path.exists():
-            self.json_path.mkdir()
-        self.dailies_path = str(self.json_path / 'dailies.json')
-        self.dailies_cog = DailiesManagementCog(self)
         self.facts_cog = FactsCog(self)
 
-        self.add_cog(self.dailies_cog)
         self.add_cog(self.facts_cog)
 
         self.run(token, bot=True, reconnect=True)
@@ -38,26 +29,17 @@ class Bot(commands.Bot):
         for guild in self.guilds:
             logging.info(guild)
 
-        self.dailies_loop.start()
+    def get_guild_from_name(self, guild_name=None):
+        if guild_name is None: guild_name = self.guild_name
+        return discord.utils.get(self.guilds, name=guild_name)
 
-    def get_guild(self):
-        pass
-        # todo make guild retrieval function
-
-    def get_role(self, roll_name):
-        guild = discord.utils.get(self.guilds, name=self.guild_name)
+    def get_role_from_name(self, roll_name):
+        guild: discord.Guild = self.get_guild_from_name()
         return discord.utils.get(guild.roles, name=roll_name)
 
-    def get_channel(self, channel_name):
-        guild: discord.Guild = discord.utils.get(self.guilds, name=self.guild_name)
+    def get_channel_from_name(self, channel_name):
+        guild: discord.Guild = self.get_guild_from_name()
         return discord.utils.get(guild.channels, name=channel_name)
-
-    def get_daily(self, daily_str=None):
-        with open(self.dailies_path, 'r') as in_file:
-            if daily_str is not None:
-                return json.load(in_file)[daily_str]
-            else:
-                return json.load(in_file)
 
     async def import_facts(self, facts_filename):
         guild = discord.utils.get(self.guilds, name=self.guild_name)
@@ -66,25 +48,5 @@ class Bot(commands.Bot):
         with open(facts_filename, 'r', encoding="utf8") as file:
             for line in file.readlines():
                 await katie_channel.send(line)
-
-    @tasks.loop(hours=24)
-    async def dailies_loop(self):
-        logging.info('dailies_loop triggered')
-        await self.dailies_cog.trigger_event(event='wins')
-
-    @dailies_loop.before_loop
-    async def before_dailies_loop(self):
-        logging.info('before dailies_loop triggered')
-        hour = self.get_daily('wins')['hour']
-        if hour > 23: hour -= 24
-        minute = 0
-
-        now = datetime.now()
-        future = datetime(now.year, now.month, now.day, hour, minute)
-
-        if now.hour >= hour and now.minute > minute:
-            future += timedelta(days=1)
-
-        await asyncio.sleep((future - now).seconds)
 
 
--- src/cogs/FactsCog.py
+++ src/cogs/FactsCog.py
@@ -10,8 +10,6 @@ from random import randrange
 from datetime import datetime
 from asyncio import sleep
 
-from Embeds import embeds
-
 WAIT_COOLDOWN = 60
 
 
@@ -20,6 +18,15 @@ class FactsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.fact_ready = True
+
+    async def get_facts(self, fact_group_str: str):
+        channel = self.bot.get_channel_from_name(fact_group_str)
+        return await channel.history(limit=123).flatten()
+
+    async def get_random_fact(self, fact_group_str: str):
+        messages = await self.get_facts(fact_group_str)
+        rand_index = randrange(0, len(messages) - 1)
+        return messages[rand_index].content
 
     @commands.command(name='getfact')
     async def getfact(self, ctx: discord.ext.commands.Context, *args):
@@ -33,23 +40,14 @@ class FactsCog(commands.Cog):
             await sleep(WAIT_COOLDOWN)
             self.fact_ready = True
 
-    async def get_facts(self, fact_group_str: str):
-        channel = self.bot.get_channel(fact_group_str)
-        return await channel.history(limit=123).flatten()
-
-    async def get_random_fact(self, fact_group_str):
-        messages = await self.get_facts(fact_group_str)
-        rand_index = randrange(0, len(messages) - 1)
-        return messages[rand_index].content
-
     @commands.command(name='addfact')
     async def addfact(self, ctx: discord.ext.commands.Context, *args):
-        fact_role = self.bot.get_role('fact adder')
+        fact_role = self.bot.get_role_from_name('fact adder')
         if len(args) < 2:
             await ctx.send('Please provide a fact.')
         if fact_role not in ctx.author.roles:
             await ctx.send('You are not allowed to do this. :)')
 
-        channel = self.bot.get_channel(args[0])
+        channel = self.bot.get_channel_from_name(args[0])
         if channel is None: return
         await channel.send(args[1])
