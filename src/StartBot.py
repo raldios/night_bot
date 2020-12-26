e211f332d97885dc63db604072f116b8eea4c24d
Violet Polaris <glavien@gmail.com>
2020-12-26 12:57:46 -0500

    check if env file exists

--- src/StartBot.py
+++ src/StartBot.py
@@ -2,10 +2,16 @@
 import os
 from sys import argv
 from dotenv import load_dotenv
+from pathlib import Path
 
 from NightBot import NightBot
 
-load_dotenv(str(argv[1]))
+env_path = Path(str(argv[1]))
+if not env_path.exists():
+    print('CRITICAL - ENV FILE NOT FOUND, EXITING')
+    exit()
+
+load_dotenv(env_path)
 
 client = NightBot(os.getenv('DISCORD_TOKEN'),
                   os.getenv('DISCORD_GUILD'),
